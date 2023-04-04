12f5bb24376aae605a7e13237c480aac3ffc7364
Never Say GoodBye <hellofeeders@gmail.com>
2023-04-04 04:32:55 +0300

    v

--- Assets/InteractableObject.cs
+++ Assets/InteractableObject.cs
@@ -1,14 +1,16 @@
 
 using Oculus.Interaction;
+using Unity.Mathematics;
 using UnityEngine;
 
 
 public class InteractableObject : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
 
     private void Start()
     {
-        var rayinteractable = GetComponent<RayInteractable>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
     public void onselectenter()
     {
@@ -18,5 +20,12 @@ public class InteractableObject : MonoBehaviour
     public void Drop()
     {
         PlayerObjectCarrier.instance.Drop();
+        _rigidbody.isKinematic = false;
+        transform.rotation = quaternion.Euler(0, transform.rotation.y, 0);
+    }
+    public void Grab()
+    {
+        _rigidbody.isKinematic = true;
+
     }
 }
